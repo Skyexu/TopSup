44a3bad22f1a7d2d9c19333a4e512dcb1bf81672
DESKTOP-IDUJKUP\Skye <xkhsky@163.com>
2018-01-09 02:19:30 +0800

    fix: phone

--- GetTitleTessAndroid.py
+++ GetTitleTessAndroid.py
@@ -19,6 +19,7 @@ img = Image.open("./screenshot.png")
 
 # 切割题目位置，左上角坐标和右下角坐标
 region = img.crop((50, 350, 1000, 560)) # 坚果 pro1
+#region = img.crop((75, 315, 1167, 789)) # iPhone 7P
 
 # tesseract 路径
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
