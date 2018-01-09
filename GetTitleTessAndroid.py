20ef61f182d17581908bc24d3c5e0630c8ccd87f
Skye <xkhsky@163.com>
2018-01-09 14:51:21 +0800

    fix: 删除已有图片，增加容错

--- GetTitleTessAndroid.py
+++ GetTitleTessAndroid.py
@@ -9,10 +9,31 @@ from PIL import Image
 import os
 import pytesseract
 import webbrowser
+import subprocess
+import numpy as np
 
 def pull_screenshot():
     os.system('adb shell screencap -p /sdcard/screenshot.png')
     os.system('adb pull /sdcard/screenshot.png .')
+    # 删除原有截图
+    os.system("adb shell rm /sdcard/screenshot.png")
+
+# 不成功待测试
+def pull_screenshot2():
+    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+    binary_screenshot = process.stdout.read()
+    img = np.array(binary_screenshot)
+    print(img)
+    f = open('screenshot.png', 'wb')
+    f.write(img)
+    f.close()
+
+# 截图已存在则删除，增加容错
+if os.path.isfile('screenshot.png'):
+    try:
+        os.remove('screenshot.png')
+    except Exception:
+        pass
 
 pull_screenshot()
 img = Image.open("./screenshot.png")
