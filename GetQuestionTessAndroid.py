8deeb3b697ae83c8955c6e0892b4b88ba2e85e09
Chenzc <chenzc@lifebuddy.cn>
2018-01-18 10:19:09 +0800

    gui

--- GetQuestionTessAndroid.py
+++ GetQuestionTessAndroid.py
@@ -9,15 +9,49 @@ from PIL import Image
 from common import screenshot, ocr, methods
 from threading import Thread
 import time
+import tkinter as tk
+from win32api import GetSystemMetrics
+from queue import Queue
+import requests
+import time
+import sys
+import os
 
-while True:
+global GUI
+global running
+running = True
+GUI = False
+argv = sys.argv;
+if (len(argv)>1 and argv[1]=='gui'):
+    GUI = True;
+
+if GUI:
+    top = tk.Tk()
+    top.geometry(str(int(GetSystemMetrics(0)/2))+'x'+str(int(GetSystemMetrics(1)/2)))
+
+def hit_me():
+    global GUI
+    global running
+    global touch_start
+    global save_choices
     # 截图
-    screenshot.check_screenshot()
+    # screenshot.check_screenshot()
 
     img = Image.open("./screenshot.png")
 
     # 文字识别
-    question, choices = ocr.ocr_img(img)
+    question, choices, selection_start = ocr.ocr_img(img)
+    touch_start = selection_start
+    save_choices = choices
+
+    result = '问题：'+question+'\n\n';
+
+    for choice in choices:
+        result += choice + '\n'
+
+    if GUI:
+        var.set(result)   # 设置标签的文字为 'you hit me'
+
     # t = time.clock()
     # 用不同方法输出结果，取消某个方法在前面加上#
 
@@ -29,17 +63,97 @@ while True:
     # methods.run_algorithm(2, question, choices)
 
     # 多线程
-    m1 = Thread(methods.run_algorithm(0, question, choices))
-    m2 = Thread(methods.run_algorithm(1, question, choices))
-    m3 = Thread(methods.run_algorithm(2, question, choices))
+    q = Queue()
+    
+    m1 = Thread(methods.run_algorithm(0, question, choices, q))
+    m2 = Thread(methods.run_algorithm(1, question, choices, q))
+    m3 = Thread(methods.run_algorithm(2, question, choices, q))
+    m4 = Thread(methods.run_algorithm(3, question, choices, q))
     m1.start()
     m2.start()
     m3.start()
+    m4.start()
 
-    # end_time = time.clock()
-    # print(end_time - t)
-    go = input('输入回车继续运行,输入 n 回车结束运行: ')
-    if go == 'n':
-        break
+    global sum
+    global paramsQuestion
+    global paramsAnswer
+    global results
+    results = {
+        'cloud_count':'',
+        'open_webbrowser_count':'',
+        'count_base':''
+    }
 
-    print('------------------------')
+    paramsQuestion = question
+    paramsAnswer = ''
+    sum=0
+    def consumer(in_q):
+        global touch_start
+        global GUI
+        global sum
+        global paramsQuestion
+        global paramsAnswer
+        global results
+        global save_choices
+        while True:
+            data = in_q.get()
+            if (data == 'END'):
+                sum = sum + 1
+            else:
+                if GUI:
+                    results[data['name']]=data['value'];
+            if (sum>2):
+                break
+        if (results['cloud_count']!=''):
+            paramsAnswer = results['cloud_count']
+        elif (results['count_base']!=''):
+            paramsAnswer = results['count_base']
+        elif (results['open_webbrowser_count']!=''):
+            paramsAnswer = results['open_webbrowser_count']
+        else:
+            paramsAnswer = '自求多福'
+
+
+        selection = 0
+        for index, item in enumerate(save_choices):
+            if item==paramsAnswer:
+                selection = index
+                break
+
+        selection_height = 170;
+
+        if GUI:
+            var.set(var.get() + '\n\n正确答案\n\n' +paramsAnswer)
+            # 模拟点按
+
+        os.system('adb shell input tap 250 ' + str(touch_start+(selection+1)*selection_height/2))
+
+        req = requests.get(url='http://localhost:3000/add', params={'question': paramsQuestion, 'answer': paramsAnswer})
+
+    t = Thread(target=consumer, args=(q,))
+    t.start()
+    if (GUI==False):
+        go = input('输入回车继续运行,输入 n 回车结束运行: ')
+        if go == 'n':
+            running = False
+
+        print('------------------------')
+
+if GUI:
+    b = tk.Button(top, 
+        text='开始',      # 显示在按钮上的文字
+        width=15, height=2, 
+        command=hit_me)     # 点击按钮式执行的命令
+    b.pack()    # 按钮位置
+    var = tk.StringVar()    # 这时文字变量储存器
+    l = tk.Label(top, 
+        textvariable=var,   # 使用 textvariable 替换 text, 因为这个可以变化
+        bg='#DDDDDD', font=('微软雅黑', 12), width=int(GetSystemMetrics(0)/2), height=int(GetSystemMetrics(1)/2)-2)
+    l.pack() 
+    # 进入消息循环
+    top.mainloop()
+else:
+    while running:
+        hit_me()
+
+
--- common/methods.py
+++ common/methods.py
@@ -7,18 +7,45 @@
 import requests
 import webbrowser
 import urllib.parse
+import json
+import time
 
 # # 颜色兼容Win 10
 from colorama import init,Fore
 init()
 
-def open_webbrowser(question):
-    webbrowser.open('https://baidu.com/s?wd=' + urllib.parse.quote(question))
+global getMax
+getMax = True
 
-def open_webbrowser_count(question,choices):
+class JSONObject:
+    def __init__(self, d):
+        self.__dict__ = d
+
+def open_webbrowser(question):
+    print("Cancel")
+    # webbrowser.open('https://www.google.co.uk/search?q=' + urllib.parse.quote(question))
+    # webbrowser.open('https://www.sogou.com/web?query=' + urllib.parse.quote(question))
+    # webbrowser.open('https://www.baidu.com/s?wd=' + urllib.parse.quote(question))
+
+def cloud_count(question, q):
+    print('\n-- 方法1： 题库筛选 --\n')
+    # q.put('\n-- 方法1： 题库筛选 --\n')
+    req = requests.get(url='http://localhost:3000/query', params={'question': question})
+    result = json.loads(req.text, object_hook=JSONObject)
+    try:
+        q.put({ 'name':'cloud_count', 'value':result.answer})
+        print(result.answer)
+    except AttributeError as e:
+        pass 
+    q.put('END')
+
+def open_webbrowser_count(question,choices,q):
+    global getMax
     print('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
+    # q.put('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
     print('Question: ' + question)
     if '不是' in question:
+        getMax = False
         print('**请注意此题为否定题,选计数最少的**')
 
     counts = []
@@ -32,10 +59,12 @@ def open_webbrowser_count(question,choices):
         count = content[:index].replace(',', '')
         counts.append(count)
         #print(choices[i] + " : " + count)
-    output(choices, counts)
+    output(choices, counts, q, 'open_webbrowser_count')
 
-def count_base(question,choices):
+def count_base(question,choices,q):
+    global getMax
     print('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
+    # q.put('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
     # 请求
     req = requests.get(url='http://www.baidu.com/s', params={'wd':question})
     content = req.text
@@ -44,12 +73,16 @@ def count_base(question,choices):
     print('Question: '+question)
     if '不是' in question:
         print('**请注意此题为否定题,选计数最少的**')
+        getMax = False
     for i in range(len(choices)):
         counts.append(content.count(choices[i]))
         #print(choices[i] + " : " + str(counts[i]))
-    output(choices, counts)
+    output(choices, counts, q, 'count_base')
 
-def output(choices, counts):
+def output(choices, counts, q, al_num):
+    global getMax
+    if (len(counts)==0):
+        return
     counts = list(map(int, counts))
     #print(choices, counts)
 
@@ -61,27 +94,37 @@ def output(choices, counts):
 
     if index_max == index_min:
         print(Fore.RED + "高低计数相等此方法失效！" + Fore.RESET)
+        q.put('END')
         return
 
     for i in range(len(choices)):
-        print()
         if i == index_max:
             # 绿色为计数最高的答案
             print(Fore.GREEN + "{0} : {1} ".format(choices[i], counts[i]) + Fore.RESET)
+            if getMax:
+                q.put({ 'name':al_num, 'value':str(choices[i])})
+
         elif i == index_min:
             # 红色为计数最低的答案
             print(Fore.MAGENTA + "{0} : {1}".format(choices[i], counts[i]) + Fore.RESET)
+            if (getMax==False):
+                q.put({ 'name':al_num, 'value':str(choices[i])})
+
         else:
             print("{0} : {1}".format(choices[i], counts[i]))
+            # q.put(' '+str(choices[i]) + ':' + str(counts[i]) + '\n')
+    q.put('END')
 
 
-def run_algorithm(al_num, question, choices):
+def run_algorithm(al_num, question, choices, q):
     if al_num == 0:
         open_webbrowser(question)
-    elif al_num == 1:
-        open_webbrowser_count(question, choices)
     elif al_num == 2:
-        count_base(question, choices)
+        open_webbrowser_count(question, choices, q)
+    elif al_num == 3:
+        count_base(question, choices, q)
+    elif al_num == 1:
+        cloud_count(question, q)
 
 if __name__ == '__main__':
     question = '新装修的房子通常哪种化学物质含量会比较高?'
